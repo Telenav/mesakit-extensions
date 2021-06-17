1a4ad57fe08b1d9df168bbfd5460ed3145d8c464
jonathan <jon@thanlocke.com>
2021-06-17 07:08:58 -0700

    🍱 Java 11+ upgrade

--- mesakit-serialization/json/src/main/java/module-info.java
+++ mesakit-serialization/json/src/main/java/module-info.java
@@ -3,5 +3,6 @@ open module mesakit.serialization.json
     requires transitive kivakit.serialization.json;
     requires transitive mesakit.graph.core;
 
+    exports com.telenav.mesakit.serialization.json;
     exports com.telenav.mesakit.serialization.json.serializers;
 }
