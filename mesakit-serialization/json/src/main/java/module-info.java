c5ce939a517448790ae1eb97ed21e07f8af50d3c
jonathan <jon@thanlocke.com>
2021-08-18 17:57:20 -0700

    💚 Fix module imports

--- mesakit-serialization/json/src/main/java/module-info.java
+++ mesakit-serialization/json/src/main/java/module-info.java
@@ -2,6 +2,7 @@ open module mesakit.serialization.json
 {
     requires transitive kivakit.serialization.json;
     requires transitive mesakit.graph.core;
+    requires transitive kivakit.component;
 
     exports com.telenav.mesakit.serialization.json;
     exports com.telenav.mesakit.serialization.json.serializers;
