48b1ef255a91221a1e7fdf932bb5d12cc8017a21
Jonathan Locke <jonathanl@telenav.com>
2022-11-29 12:13:24 -0700

    Bugfix/demodularize (#15)
    * 🚧 work in progress on demodularizing kivakit
    
    * 🐛 Fix remaining tests
    
    * 👷 Update javadoc paths for antlr4

--- mesakit-tools/applications/pbf-comparator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module mesakit.tools.applications.pbf.comparator
-{
-    requires mesakit.data.formats.pbf;
-
-    requires transitive kivakit.application;
-
-    exports com.telenav.mesakit.tools.applications.pbf.comparator;
-}
